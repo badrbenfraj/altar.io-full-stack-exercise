be61a1b1d6687dba83060d29a225cf84894774e4
Badredine BENFRAJ <bbenfraj@vermeg.com>
2024-10-14 20:01:33 +0100

    fix: [ALTAR-0021] 🐞 socket endpoint docker

--- apps/frontend/src/app/shared/shared.module.ts
+++ apps/frontend/src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { LiveComponent } from '@ui/components';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: '', options: {} };
 
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule, LiveComponent, SocketIoModule.forRoot(config)],
